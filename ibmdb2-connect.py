fa715f4a1356dfb696fea3cd3dacbd4ada169854
anusha4011457 <114054853+anusha4011457@users.noreply.github.com>
2022-10-19 09:19:47 -0700

    Delete ibmdb2-connect.py

--- ibmdb2-connect.py
+++
@@ -1,4 +0,0 @@
-
-import ibm_db
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31864;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xcx38812;PWD=PCeNezAAxo5ETvkb",'','')
-print(conn)
