be67433b3c1130dc4770f6c43f5bf54acfbbcbea
anusha4011457 <114054853+anusha4011457@users.noreply.github.com>
2022-10-19 09:22:15 -0700

    Delete sqlite_db_setup.py

--- sqlite_db_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect("detail.db")
-print("opened database successfully")
-
-conn.execute('create table detail(name text,email text,phone text,password text')
-print("Table created successfully")
-conn.close()
